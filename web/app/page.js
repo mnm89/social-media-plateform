c6b793d04efa2c34205c670ca991458837c0a6b5
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-11-03 13:39:08 +0100

    bootstrap web app

---
+++ web/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to the Social Media Platform
+      </h1>
+    </div>
+  );
+}
