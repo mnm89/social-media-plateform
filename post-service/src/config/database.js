c5c91f684b5eaebc8499246bd93c7b0950bbfbd6
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-11-06 19:00:28 +0100

    update db config

--- post-service/src/config/database.js
+++ post-service/src/config/database.js
@@ -1,4 +1,23 @@
 module.exports = {
-  use_env_variable: "DATABASE_URL",
-  dialect: "postgres",
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    logging: console.log, // Logs SQL queries in development
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    logging: false,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Adjust based on SSL requirements
+      },
+    },
+  },
 };
