bb2443bbcce294158397f2cdf61d7c3a16c7674e
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-11-30 16:58:19 +0100

    unused import

--- web/components/main-nav.tsx
+++ web/components/main-nav.tsx
@@ -3,9 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 
 import { NavItem } from "@/types/nav"
-import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
 
 interface MainNavProps {
   items?: NavItem[]
