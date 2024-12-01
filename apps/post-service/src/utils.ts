0e103a11441d4e3275db25ab4d65b5c1f47c88fc
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-12-01 10:32:41 +0100

    fix get user from post service

--- apps/post-service/src/utils.ts
+++ apps/post-service/src/utils.ts
@@ -1,38 +1,30 @@
 import { cache } from './config';
-import {
-  getAccessToken,
-  getKeycloakUser,
-} from '@social-media-platform/keycloak-utils';
+import { getAccessToken } from '@social-media-platform/keycloak-utils';
+
+export async function getUser(id: string) {
+  const token = await getAccessToken();
+  const response = await fetch(`${process.env.USER_SERVICE_URL}/users/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (response.ok) return response.json();
+}
 
 export async function getUserName(id: string) {
   const cached = await cache.get(`user:${id}`);
   if (cached) return JSON.parse(cached).username;
-  const user = await getKeycloakUser(id);
-  if (user) {
-    cache.set(`user:${id}`, JSON.stringify(user));
-    return user.username;
-  }
+  const user = await getUser(id);
+  if (user) return user.username;
 }
 
 export async function getUserAvatarUrl(id: string) {
   const cached = await cache.get(`avatar:${id}`);
   if (cached) return JSON.parse(cached).url;
-  const token = await getAccessToken();
-  const response = await fetch(
-    `${process.env.FILE_SERVICE_URL}/avatars/${id}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-  if (response.ok) {
-    const json = await response.json();
-    cache.set(`avatar:${id}`, JSON.stringify(json)); // Cache the storage model
-    return json.url;
-  }
+  const user = await getUser(id);
+  if (user) return user.avatar;
 }
 export async function isFriend(userId, friendId) {
   const [first, second] = [userId, friendId].sort();
